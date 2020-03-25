848cabb9f5d321274295612a4d0ca5df29119bec
Dragan Pepic <d.pepic@hotmail.com>
2020-03-25 21:52:44 +0100

    Add project files.

---
+++ ChatDrugiPut/Shared/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace ChatDrugiPut.Shared
+{
+	public class User
+	{
+		[Required]
+		[StringLength(50, MinimumLength = 5, ErrorMessage ="Check length!")]
+		public string Username { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 5, ErrorMessage = "Check length (password:D)!")]
+		public string Password { get; set; }
+		public bool LoggedIn { get; set; }
+	}
+}
