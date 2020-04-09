91f57560245fe1d348ce7ad7c16865833be4229f
Dragan Pepic <d.pepic@hotmail.com>
2020-04-09 19:04:16 +0200

    asd

--- ChatDrugiPut/Server/EF/Baza.cs
+++ ChatDrugiPut/Server/EF/Baza.cs
@@ -25,6 +25,7 @@ namespace ChatDrugiPut.Server.EF
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
+			
 			optionsBuilder.UseSqlServer("Data Source=DESKTOP-75VO5EN;Initial Catalog = ChatBaza;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 		}
 	}
